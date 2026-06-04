4d8bd8f9e4da215514b83eab7177761cc5f9a977
小狼 <b2blacks@gmail.com>
2026-06-04 20:20:03 +0800

    set logo to 200x43

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,8 +17,8 @@ export function Header({ title, showLogo, showBack, onBack }: HeaderProps) {
         <Image
           src="/haogu-icon.png"
           alt="好顧"
-          width={420}
-          height={90}
+          width={200}
+          height={43}
           priority
         />
       </div>
